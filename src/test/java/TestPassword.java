f6537c74786e95660325b0bb60a3b839959500b7
suhaib1198 <suhaibbase9@gmail.com>
2022-10-01 15:00:53 +0300

    selenuim pssword2

--- src/test/java/TestPassword.java
+++ src/test/java/TestPassword.java
@@ -1,4 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -9,24 +15,34 @@ import uplode_trello.LogTrello;
 public class TestPassword {
 	static WebDriver driver;
 	LogTrello l;
-	
-	
+	FileReader file ;
+	Properties prop;
 	@BeforeSuite
-	public void before() {
+	public void before() throws FileNotFoundException {
 	
 	driver = OpenBrowsers.openBrowser("chrome");
 	driver.get("https://trello.com/login");
 	driver.manage().window().maximize();
 	l=new LogTrello(driver);
+    file =new FileReader("./files/user.proparoties");
+	prop=new Properties();
+	
 	}
 	
 	@Test
-	public void login() throws InterruptedException {
+	public void login() throws InterruptedException, IOException {
 	
-	l.login("suhaibbase9@gmail.com", "Su1234567");
 	
 	
 }
+	@Test
+	public void test22() throws IOException, InterruptedException {
+	prop.load(file);
+	String us=prop.getProperty("user");
+	String pass= prop.getProperty("password");
+	l.login(us, pass);	
+	Assert.assertEquals(us,"useres");
+	}
 	@AfterSuite
 	public void after() {
 		driver.quit();
